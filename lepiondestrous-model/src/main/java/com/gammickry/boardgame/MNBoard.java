02c3b4668bfb61098c8b1bcc83f49b56a5204a35
Octavian Nita <mnothro@yahoo.com>
2015-05-07 17:39:58 +0200

    complete, minimal mxn board abstraction + tests

--- lepiondestrous-model/src/main/java/com/gammickry/boardgame/MNBoard.java
+++ lepiondestrous-model/src/main/java/com/gammickry/boardgame/MNBoard.java
@@ -1,10 +1,13 @@
 package com.gammickry.boardgame;
 
+import java.util.Arrays;
+
 import static java.lang.System.arraycopy;
 
 /**
  * Representation of a m×n board used in <a href="http://en.wikipedia.org/wiki/M,n,k-game">m,n,k-games</a> and
- * other <a href="http://en.wikipedia.org/wiki/Abstract_strategy_game">abstract strategy (board) games</a>.
+ * other <a href="http://en.wikipedia.org/wiki/Abstract_strategy_game">abstract strategy (board) games</a>. The
+ * implementation is not thread-safe.
  *
  * @author Octavian Theodor NITA (http://github.com/octavian-nita)
  * @version 1.0, May 06, 2015
@@ -18,6 +21,8 @@ public class MNBoard {
     protected final int rows;
 
     protected final int[] grid;
+
+    public MNBoard(int size) { this(size, size); }
 
     public MNBoard(int cols, int rows) {
         if (cols <= 0) {
@@ -33,40 +38,83 @@ public class MNBoard {
 
     public MNBoard(MNBoard board) {
         if (board == null) {
-            throw new IllegalArgumentException("cannot clone a null board");
+            throw new IllegalArgumentException("cannot copy a null board");
         }
-        this.cols = board.cols;
-        this.rows = board.rows;
-        this.grid = new int[this.rows * this.cols];
-        arraycopy(board.grid, 0, this.grid, 0, this.grid.length);
+        cols = board.cols;
+        rows = board.rows;
+        grid = new int[rows * cols];
+        arraycopy(board.grid, 0, grid, 0, grid.length);
     }
 
-    public boolean onTheBoard(int col, int row) {
+    public final boolean onTheBoard(int col, int row) {
         return 0 <= col && col < cols && 0 <= row && row < rows;
     }
 
     public boolean empty(int col, int row) {
         if (!onTheBoard(col, row)) {
-            throw new IllegalArgumentException("the specified position is outside of the board");
+            throw new IllegalArgumentException("the specified location is off the board");
         }
         return grid[rows * row + col] == EMPTY;
     }
 
     public MNBoard place(int col, int row, int piece) {
+        if (!onTheBoard(col, row)) {
+            throw new IllegalArgumentException("the specified location is off the board");
+        }
         if (piece == EMPTY) {
             throw new IllegalArgumentException("cannot place an 'empty' piece");
         }
+
         grid[rows * row + col] = piece;
+
         return this;
     }
 
     public int displace(int col, int row) {
         if (!onTheBoard(col, row)) {
-            throw new IllegalArgumentException("the specified position is outside of the board");
+            throw new IllegalArgumentException("the specified location is off the board");
         }
-        int pos = rows * row + col;
-        int piece = grid[pos];
-        grid[pos] = EMPTY;
+
+        int loc = rows * row + col;
+        int piece = grid[loc];
+        grid[loc] = EMPTY;
+
         return piece;
+    }
+
+    public MNBoard move(int fromCol, int fromRow, int toCol, int toRow) {
+        return place(toCol, toRow, displace(fromCol, fromRow));
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        MNBoard board = (MNBoard) object;
+
+        if (cols != board.cols) {
+            return false;
+        }
+        if (rows != board.rows) {
+            return false;
+        }
+        if (!Arrays.equals(grid, board.grid)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = cols;
+        result = 31 * result + rows;
+        result = 31 * result + Arrays.hashCode(grid);
+        return result;
     }
 }
