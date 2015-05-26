e1b09e5cac8275941b4a079ac98ffeb6841d003d
octavian <octavian.nita@gmail.com>
2015-05-27 01:35:18 +0200

    cleaned up the web UI template

---
+++ lepiondestrous-web/js/lepiondestrous.js
@@ -0,0 +1,11 @@
+/*jshint browser: true, devel: true, indent: 2, maxerr: 50, maxlen: 120 */
+/*global define: true, module: true, exports: true */
+
+window.addEventListener('load', function (event) {
+  'use strict';
+
+  var boardSize = 14;
+
+  // TODO: Define the game UI
+
+}, false);
