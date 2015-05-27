f41bbb94fc1c6fa12b92549f50c901da2db3c833
Octavian Nita <mnothro@yahoo.com>
2015-05-27 16:01:44 +0200

    creating the web UI game view

--- lepiondestrous-web/js/lepiondestrous.js
+++ lepiondestrous-web/js/lepiondestrous.js
@@ -1,11 +1,19 @@
 /*jshint browser: true, devel: true, indent: 2, maxerr: 50, maxlen: 120 */
 /*global define: true, module: true, exports: true */
 
+//
+// 01. Define the game play (gameplay)
+// 02. Define the game view / graphics
+// 03. Instantiate the game view and draw it
+//
+
 window.addEventListener('load', function (event) {
   'use strict';
 
-  var boardSize = 14;
+  /** @constructor */
+  function GameView(gameOptions) {}
 
-  // TODO: Define the game UI
+  GameView.prototype.draw = function () {}
 
+  new GameView().draw();
 }, false);
