5999b2b31066306c8aa2daf74a5436671bd2f127
Octavian Nita <mnothro@yahoo.com>
2015-05-28 18:15:16 +0200

    creating the web UI game view

--- lepiondestrous-web/js/lepiondestrous.js
+++ lepiondestrous-web/js/lepiondestrous.js
@@ -10,19 +10,47 @@
 window.addEventListener('load', function (event) {
   'use strict';
 
+  var
+    THEME = {
+      boardDark: '#795548',
+      boardLight: '#d7ccc8'
+    },
+    BOARD_RATIO = 29 / 40;
+
   /** @constructor */
   function GameView(parent, options) {
-    var width, height;
+    var cnv, ctx, boundsRatio, edge;
 
     if (!(this instanceof GameView)) { return new GameView(parent, options); }
 
     if (!parent) { return; }
     if (!options) { options = {}; }
 
-    width = parent.offsetWidth || 435;
-    height = parent.offsetHeight || 600;
+    boundsRatio = parent.offsetWidth / parent.offsetHeight;
 
-    // display vertical board and ignore orientation for the moment
+    cnv = document.createElement('canvas');
+    cnv.width = parent.offsetWidth;
+    cnv.height = parent.offsetHeight;
+
+    ctx = cnv.getContext('2d');
+    //ctx.translate(0.5, 0.5);
+
+    ctx.fillStyle = THEME.boardLight;
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 2;
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
+
+    /*ctx.fillStyle = THEME.boardDark;
+    ctx.fillRect(0, 20, cnv.width, 120);
+    ctx.restore();*/
+
+    ctx.fillStyle = THEME.boardDark;
+    ctx.fillRect(50, 190, 150, 120);
+
+    parent.appendChild(cnv);
   }
 
   GameView.prototype.draw = function () {};
