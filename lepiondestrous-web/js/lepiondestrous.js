5ab07370df77118771db3ba1a5cdf359db487fef
Octavian Nita <mnothro@yahoo.com>
2015-06-05 17:35:46 +0200

    improved graphics with canvas over-sampling

--- lepiondestrous-web/js/lepiondestrous.js
+++ lepiondestrous-web/js/lepiondestrous.js
@@ -5,13 +5,16 @@ window.addEventListener('load', function (event) {
   'use strict';
 
   var
+    R = {
+      gameTitle: 'Le pion des trous'
+    },
     T = { // http://www.materialui.co/colors
-      label: 'Le pion des trous',
-      text: '#ffc107',
+      fontFamily: '"Chantelli Antiqua"',
+      textColor: '#ffc107',
       boardDark: '#795548',
       boardLight: '#8d6e63',
-      raisedEffect: {offsetX: 0, offsetY: 2, blur: 10, color: 'rgba(0, 0, 0, 0.9)'},
-      loweredEffect: {}
+      effectRaised: {offsetX: 0, offsetY: 2, blur: 15, color: 'rgba(0, 0, 0, 0.9)'},
+      effectLowered: {}
     };
 
   /** @constructor */
@@ -36,90 +39,77 @@ window.addEventListener('load', function (event) {
     this.board.y = (parent.offsetHeight - this.board.height ) / 2;
     this.board.unit = this.board.height / 19;
 
+    // The canvas is over-sampled and fills its parent:
     this.canvas = document.createElement('canvas');
-    this.canvas.width = parent.offsetWidth;   // the canvas fills
-    this.canvas.height = parent.offsetHeight; // ... its parent
+    this.canvas.width = parent.offsetWidth * 2;
+    this.canvas.height = parent.offsetHeight * 2;
+    this.canvas.style.width = parent.offsetWidth + 'px';
+    this.canvas.style.height = parent.offsetHeight + 'px';
+    this.canvas.getContext('2d').scale(2, 2);
 
     this.render();
-    parent.innerHTML = '';
+    parent.innerHTML = ''; // empty parent content
     parent.appendChild(this.canvas);
   }
 
   /** Consider http://www.mobtowers.com/html5-canvas-crisp-lines-every-time/ when drawing. */
   GameView.prototype.render = function () {
     if (!this.canvas || !this.board) { return; }
-    var
-      ctx = this.canvas.getContext('2d'),
-      brd = this.board, u = brd.unit,
-      y0 = 5 * u, cr = 1.6 * u, cy = 3.4 * u,
-      tm;
+    var ctx = this.canvas.getContext('2d'), brd = this.board;
     ctx.save();
 
     ctx.fillStyle = T.boardLight;
     ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-    ctx.lineWidth = 2;
+    ctx.shadowOffsetX = T.effectRaised.offsetX;
+    ctx.shadowOffsetY = T.effectRaised.offsetY;
+    ctx.shadowColor = T.effectRaised.color;
+    ctx.shadowBlur = T.effectRaised.blur;
+
     ctx.fillStyle = T.boardDark;
-    ctx.strokeStyle = T.text;
-    ctx.shadowOffsetX = T.raisedEffect.offsetX;
-    ctx.shadowOffsetY = T.raisedEffect.offsetY;
-    ctx.shadowBlur = T.raisedEffect.blur;
-    ctx.shadowColor = T.raisedEffect.color;
-
     ctx.fillRect(brd.x, brd.y, brd.width, brd.height);
-    ctx.translate(brd.x, brd.y); // move the origin to the board top left corner
-    ctx.shadowBlur /= (brd.unit / 10);
 
-    // [DEBUG grid]
-    /*ctx.save();
-     ctx.strokeStyle = '#646464';
-     ctx.beginPath();
-     for (var i = 0; i < 13; i++) {
-     ctx.moveTo((i + 1) * u, 0);
-     ctx.lineTo((i + 1) * u, brd.height);
-     }
-     for (i = 0; i < 18; i++) {
-     ctx.moveTo(0, (i + 1) * u);
-     ctx.lineTo(brd.width, (i + 1) * u);
-     }
-     ctx.stroke();
-     ctx.restore();*/
-    // [/DEBUG]
+    ctx.translate(brd.x, brd.y);   // move the origin to the board top left corner
+    ctx.scale(brd.unit, brd.unit); // draw the board elements in terms of units
+
+    ctx.lineWidth = 2 / brd.unit;
+    ctx.strokeStyle = T.textColor;
+    ctx.shadowBlur /= (brd.unit / 15); // stronger shadow
 
     // Board decoration:
     ctx.beginPath();
-    ctx.moveTo(0, y0);
-    ctx.lineTo(2.5 * u, y0);
+    ctx.moveTo(0, 5);
+    ctx.lineTo(2.5, 5);
 
     // First small arch:
-    ctx.arcTo(2.6 * u, cy, 3.5 * u, 3 * u, cr);
-    ctx.arcTo(4.4 * u, cy, 4.5 * u, y0, cr);
-    ctx.lineTo(4.5 * u, y0); // finish the arch
-    ctx.lineTo(5.5 * u, y0);
+    ctx.arcTo(2.6, 3.4, 3.5, 3, 1.6);
+    ctx.arcTo(4.4, 3.4, 4.5, 5, 1.6);
+    ctx.lineTo(4.5, 5); // finish the arch
+    ctx.lineTo(5.5, 5);
     // Middle large arch:
-    ctx.arcTo(5.6 * u, 2.95 * u, 7 * u, 2.5 * u, cr + 0.5 * u);
-    ctx.arcTo(8.4 * u, 2.95 * u, 8.5 * u, y0, cr + 0.5 * u);
-    ctx.lineTo(8.5 * u, y0); // finish the arch
-    ctx.lineTo(9.5 * u, y0);
+    ctx.arcTo(5.6, 2.95, 7, 2.5, 1.6 + 0.5);
+    ctx.arcTo(8.4, 2.95, 8.5, 5, 1.6 + 0.5);
+    ctx.lineTo(8.5, 5); // finish the arch
+    ctx.lineTo(9.5, 5);
 
     // Second small arch:
-    ctx.arcTo(9.6 * u, cy, 10.5 * u, 3 * u, cr);
-    ctx.arcTo(11.4 * u, cy, 11.5 * u, y0, cr);
-    ctx.lineTo(11.5 * u, y0); // finish the arch
-    ctx.lineTo(14 * u, y0);
+    ctx.arcTo(9.6, 3.4, 10.5, 3, 1.6);
+    ctx.arcTo(11.4, 3.4, 11.5, 5, 1.6);
+    ctx.lineTo(11.5, 5); // finish the arch
+    ctx.lineTo(14, 5);
     ctx.stroke(); // finish the bridge lower outline
 
     // Bridge top:
     ctx.beginPath();
-    ctx.moveTo(3 * u, 0);
-    ctx.lineTo(3 * u, 2 * u);
-    ctx.lineTo(11 * u, 2 * u);
-    ctx.lineTo(11 * u, 0);
+    ctx.moveTo(3, 0);
+    ctx.lineTo(3, 1.5);
+    ctx.lineTo(11, 1.5);
+    ctx.lineTo(11, 0);
     ctx.stroke();
 
-    ctx.font = (brd.unit * 8 / 9) + 'px \'Chantelli Antiqua\'';
-    ctx.fillStyle = T.text;
-    ctx.fillText(T.label, (brd.width - ctx.measureText(T.label).width) / 2, 1.3 * u);
+    ctx.font = 0.8 + 'px ' + T.fontFamily;
+    ctx.fillStyle = T.textColor;
+    ctx.fillText(R.gameTitle, (brd.width - ctx.measureText(R.gameTitle).width) / 2, 1.3);
 
     ctx.restore();
   };
