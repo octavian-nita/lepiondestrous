2f6fc9d7dd013a8ceed11c5a77d1df64b5b25cfe
Octavian Nita <mnothro@yahoo.com>
2015-06-09 18:07:27 +0200

    still working on the holes

--- lepiondestrous-web/js/lepiondestrous.js
+++ lepiondestrous-web/js/lepiondestrous.js
@@ -6,12 +6,11 @@ window.addEventListener('load', function (/*event*/) {
 
   var
 
-    O = { // Game view default options
+    O = { // Game default options
+      gameSize: 14,
       board: {
         ratio: 14 / 19,
-        heightUnits: 19,
         maxHeight: 695,
-        widthUnits: 14,
         maxWidth: 504
       }
     },
@@ -23,11 +22,11 @@ window.addEventListener('load', function (/*event*/) {
     T = { // Game theme; http://www.materialui.co/colors
       fontFamily: '"Chantelli Antiqua"',
       textColor: '#ffc107',
-      holeColor: '#4e342e'/* '#5d4037'*/,
+      holeColor: '#4e342e',
       boardDark: '#795548',
       boardLight: '#8d6e63',
       effectRaised: {offsetX: 0, offsetY: 2, blur: 20, color: 'rgba(0, 0, 0, 0.9)'},
-      effectLowered: {offsetX: 0, offsetY: -2, blur: 20, color: 'rgba(0, 0, 0, 0.9)'}
+      effectLowered: {}
     };
 
   /** @constructor */
@@ -44,27 +43,27 @@ window.addEventListener('load', function (/*event*/) {
      */
     this._board = {};
 
+    // Set up the board geometry (always vertical for the moment):
+    var boardRatio = opts.board.ratio || O.board.ratio, boundsRatio = parent.offsetWidth / parent.offsetHeight;
+    if (boardRatio > boundsRatio) { // http://www.frontcoded.com/javascript-fit-rectange-into-bounds.html
+      this._board.width = Math.min(parent.offsetWidth, opts.board.maxWidth || O.board.maxWidth);
+      this._board.height = this._board.width / boardRatio;
+    } else {
+      this._board.height = Math.min(parent.offsetHeight, opts.board.maxHeight || O.board.maxHeight);
+      this._board.width = this._board.height * boardRatio;
+    }
+    this._board.x = (parent.offsetWidth - this._board.width ) / 2;
+    this._board.y = (parent.offsetHeight - this._board.height) / 2;
+    this._board.unit = this._board.height * boardRatio / (opts.gameSize || O.board.gameSize);
+
     /**
-     * The canvas on which the game is drawn is over-sampled (2x) and fills its parent.
+     * The canvas on which the game is drawn is oversampled (2x) and fills its parent.
      *
      * @type {HTMLCanvasElement}
      */
     this._canvas = document.createElement('canvas');
 
-    // Set up the board geometry:
-    var boardRatio = opts.board.ratio || O.board.ratio, boundsRatio = parent.offsetWidth / parent.offsetHeight;
-    if (boardRatio > boundsRatio) { // http://www.frontcoded.com/javascript-fit-rectange-into-bounds.html
-      this._board.width = Math.min(parent.offsetWidth, (opts.board.maxWidth || O.board.maxWidth));
-      this._board.height = this._board.width / boardRatio;
-    } else {
-      this._board.height = Math.min(parent.offsetWidth, opts.board.maxHeight || O.board.maxHeight);
-      this._board.width = this._board.height * boardRatio;
-    }
-    this._board.x = (parent.offsetWidth - this._board.width ) / 2;
-    this._board.y = (parent.offsetHeight - this._board.height ) / 2;
-    this._board.unit = this._board.height / (opts.board.heightUnits || O.board.heightUnits);
-
-    // Set up an over-sampled (2x) canvas:
+    // Set up an oversampled (2x) canvas:
     this._canvas.width = parent.offsetWidth * 2;
     this._canvas.height = parent.offsetHeight * 2;
     this._canvas.style.width = parent.offsetWidth + 'px';
@@ -83,6 +82,7 @@ window.addEventListener('load', function (/*event*/) {
 
   GameView.prototype._renderBoard = function () {
     if (!this._canvas || !this._board) { return; }
+
     var ctx = this._canvas.getContext('2d'), brd = this._board;
     ctx.save();
 
@@ -100,15 +100,15 @@ window.addEventListener('load', function (/*event*/) {
     ctx.translate(brd.x, brd.y);   // move the origin to the board top left corner
     ctx.scale(brd.unit, brd.unit); // draw the board elements in terms of units
 
-    ctx.lineWidth = 2 / brd.unit;
+    ctx.shadowBlur /= brd.unit / 10; // stronger shadow
     ctx.strokeStyle = T.textColor;
-    ctx.shadowBlur /= (brd.unit / 15); // stronger shadow
+    ctx.lineWidth = 2 / brd.unit;    // the canvas is oversampled
 
-    // Board decoration:
+    ctx.translate(0, -0.2);
+    // Arch Bridge:
     ctx.beginPath();
     ctx.moveTo(0, 5);
     ctx.lineTo(2.5, 5);
-
     // First small arch:
     ctx.arcTo(2.6, 3.4, 3.5, 3, 1.6);
     ctx.arcTo(4.4, 3.4, 4.5, 5, 1.6);
@@ -124,7 +124,8 @@ window.addEventListener('load', function (/*event*/) {
     ctx.arcTo(11.4, 3.4, 11.5, 5, 1.6);
     ctx.lineTo(11.5, 5); // finish the arch
     ctx.lineTo(14, 5);
-    ctx.stroke(); // finish the bridge lower outline
+    ctx.stroke();
+    ctx.translate(0, 0.2);
 
     // Bridge top:
     ctx.beginPath();
@@ -143,24 +144,26 @@ window.addEventListener('load', function (/*event*/) {
 
   GameView.prototype._renderHoles = function () {
     if (!this._canvas || !this._board) { return; }
+
     var
-      ctx = this._canvas.getContext('2d'), brd = this._board, gameSize = O.board.widthUnits,
-      r = (brd.unit - 15) / brd.unit, i, j, pi2 = Math.PI * 2;
+      ctx = this._canvas.getContext('2d'), brd = this._board, gameSize = O.gameSize, pi2 = Math.PI * 2,
+      i, j, g, cx, cy;
     ctx.save();
 
     ctx.translate(brd.x, brd.y);   // move the origin to the board top left corner
     ctx.scale(brd.unit, brd.unit); // draw the board elements in terms of units
 
-    //    ctx.shadowOffsetX = T.effectLowered.offsetX;
-    //    ctx.shadowOffsetY = T.effectLowered.offsetY;
-    //    ctx.shadowColor = T.effectLowered.color;
-    //    ctx.shadowBlur = T.effectLowered.blur / (brd.unit / 15); // stronger shadow
-    ctx.fillStyle = T.holeColor;
-
     for (i = 0; i < gameSize; i++) {
       for (j = 0; j < gameSize; j++) {
+        cx = j + 0.5;
+        cy = i + 5.5;
+
+        ctx.fillStyle = ctx.createRadialGradient(cx, cy + 0.1, 0.1, cx, cy, 0.3);
+        ctx.fillStyle.addColorStop(0, '#4e342e');
+        ctx.fillStyle.addColorStop(1, '#3e2723');
+
         ctx.beginPath();
-        ctx.arc(j + 0.5, i + 5.5, r, 0, pi2);
+        ctx.arc(cx, cy, 0.3, 0, pi2);
         ctx.fill();
       }
     }
