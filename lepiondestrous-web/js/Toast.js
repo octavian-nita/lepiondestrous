66e6f9e09de9dad23c43bb0339e7b09d2ba6b747
Octavian Nita <mnothro@yahoo.com>
2015-09-09 18:22:14 +0200

    refactoring === chaos...

--- lepiondestrous-web/js/Toast.js
+++ lepiondestrous-web/js/Toast.js
@@ -4,6 +4,17 @@ define(
   function (cfg) {
     'use strict';
 
+    function prefixCss(elementOrStyle, propertyName, value) {
+      if (!elementOrStyle) { return; }
+
+      var style = elementOrStyle instanceof HTMLElement ? elementOrStyle.style : elementOrStyle;
+      style[propertyName] =
+      style['-o-' + propertyName] =
+      style['-ms-' + propertyName] =
+      style['-moz-' + propertyName] =
+      style['-webkit-' + propertyName] = value;
+    }
+
     /**
      * @author Octavian Theodor NITA (http://github.com/octavian-nita)
      * @version 1.0, August 24, 2015
@@ -18,20 +29,31 @@ define(
     function Toast(className, zIndex) {
       if (!(this instanceof Toast)) { return new Toast(className, zIndex); }
 
+      this._timeout = Number(cfg.toastTimeout) || 750;
+      this._easeDuration = Number(cfg.toastEaseDuration) || 3000;
+      this._transition = 'opacity ' + this._easeDuration + 'ms ease';
+
       Object.defineProperty(this, 'element', { enumerable: true, value: document.createElement('div') });
       if (className) { this.element.className = className + ''; }
 
       var style = this.element.style;
+      style.zIndex = Number(zIndex) || 9999;
+      style.pointerEvents = 'none';
 
       style.position = 'absolute';
-      style.zIndex = Number(zIndex) || 9999;
-
       style.top = '65%';
       style.left = '50%';
-      style.transform = 'translateX(-50%)';
+      style.transform =
+      style['-o-transform'] =
+      style['-ms-transform'] =
+      style['-moz-transform'] =
+      style['-webkit-transform'] = 'translateX(-50%)';
 
       style.borderRadius = '75px';
       style.padding = '0 25px';
+      style.maxHeight = '30%';
+
+      style.overflow = 'hidden';
 
       style.color = cfg.theme.foreground;
       style.background = 'rgba(0, 0, 0, 0.7)';
@@ -39,36 +61,53 @@ define(
 
       style.display = 'none';
       style.opacity = 0;
-      style['pointer-events'] = 'none';
     }
 
     Toast.prototype.show = function (message, timeout) {
-      var elem = this.element, style = elem.style;
+      var element = this.element, style = element.style,
+          transition = this._transition, timeout = Number(timeout) || this._timeout;
 
-      if (window.getComputedStyle(elem).display != 'none') {
-        elem.innerHTML += '<p>' + message + '</p>';
-      } else {
-        elem.innerHTML = '<p>' + message + '</p>';
+      if (window.getComputedStyle(element).display != 'none') {
+        element.innerHTML += '<p>' + message + '</p>';
+        return;
+      }
 
-        style.display = 'block';
-        style.opacity = 1;
+      element.innerHTML = '<p>' + message + '</p>';
+
+      style.display = 'block';
+      style.opacity = 1;
+
+      setTimeout(function () {
+
+        style.transition =
+        style['-o-transition'] =
+        style['-ms-transition'] =
+        style['-moz-transition'] =
+        style['-webkit-transition'] = transition;
+
+        style.opacity = 0;
         setTimeout(function () {
-
+          style.display = 'none';
           style.transition =
           style['-o-transition'] =
+          style['-ms-transition'] =
           style['-moz-transition'] =
-          style['-webkit-transition'] = 'opacity ' + (Number(cfg.toastEaseDuration) || 3000) + 'ms ease';
-
-          style.opacity = 0;
-          setTimeout(function () {
-            style.display = 'none';
-          }, Number(cfg.toastEaseDuration) || 3000);
-        }, Number(timeout) || Number(cfg.toastTimeout) || 750);
-      }
+          style['-webkit-transition'] = 'none';
+        }, Number(cfg.toastEaseDuration) || 3000);
+      }, timeout);
     };
 
     Toast.prototype.cancel = function () {
+      var element = this.element, style = element.style;
 
+      element.innerHTML = '';
+      style.display = 'none';
+      style.opacity = 0;
+      style.transition =
+      style['-o-transition'] =
+      style['-ms-transition'] =
+      style['-moz-transition'] =
+      style['-webkit-transition'] = 'none';
     };
 
     return Toast;
